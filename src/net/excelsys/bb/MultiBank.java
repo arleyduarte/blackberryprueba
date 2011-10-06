3b2c850d066b5d33f4c5cebe33ddd61917c41530
Arley Duarte <arley.duarte@excelsys.net>
2011-10-06 09:47:11 -0500

    Test

---
+++ src/net/excelsys/bb/MultiBank.java
@@ -0,0 +1,10 @@
+package net.excelsys.bb;
+
+public class MultiBank{
+    /**
+     * Entry point for application
+     * @param args Command line arguments (not used)
+     */ 
+    public static void main(String[] args){
+    }
+}
